838fcf0827593aa74caf9ec6c8adad171147b240
harshityadaav <harshit2622002@gmail.com>
2024-07-19 14:41:01 +0530

    Add Resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -17,7 +17,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/harshityadaav",
   resume:
-    "",
+    "https://drive.google.com/file/d/1dCqnxwjpAmRbhG5R96dphfmErxcWaSvu/view",
   linkedin: "https://www.linkedin.com/in/harshit-yadaav/",
   twitter: "https://x.com/HarshitYad70357",
   insta: "https://www.instagram.com/harshityadaav/",
